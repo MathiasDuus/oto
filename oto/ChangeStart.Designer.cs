19d110a4da9f6e9f3b8efb9955568a4d2ad0e94d
Mathias Duus <mathias031001@gmail.com>
2021-07-28 15:10:54 +0200

    V. 1.1.1 Discovered I had forgot to translate a little popup text. Removed the "unable to find manifest signing certificate in the certificate store" error

--- oto/ChangeStart.Designer.cs
+++ oto/ChangeStart.Designer.cs
@@ -39,9 +39,9 @@ namespace oto
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(0, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(194, 29);
+            this.label1.Size = new System.Drawing.Size(176, 29);
             this.label1.TabIndex = 1;
-            this.label1.Text = "Tryk på en knap";
+            this.label1.Text = "Press a button";
             // 
             // textBox1
             // 
